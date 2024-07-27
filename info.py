2c699d49fd41a745a7bc4d13d3fda374fbd7c9d1
Swaraj16090 <156641910+Swaraj16090@users.noreply.github.com>
2024-07-27 11:16:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://imax01:<imax01>@cluster0.ywoguz7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "imax01")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
